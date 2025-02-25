5f979b4c368e3a27905d6ceaff7d048662375f0b
AngelAyonV <angelayon815@gmail.com>
2025-02-24 21:11:35 -0700

    cascaron hecho

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import BusinessPageApp from './BusinessPageApp.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles.css"; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,29 @@
 
 const HomePage = () => {
-  return (
-    <div>
-        <h1>Welcome to our business page</h1>
-        <h2>Business page content </h2>
-    </div>
-
-  )
+    return (
+        <section id="home" className="container vh-100 d-flex align-items-center">
+          <div className="row w-100">
+            {/* Texto a la izquierda */}
+            <div className="col-md-6 d-flex flex-column justify-content-center">
+              <h1 className="fw-bold">Bienvenido a Nuestra Empresa</h1>
+              <p className="text-muted">
+                Ofrecemos soluciones innovadoras para tu negocio. ¡Conócenos!
+              </p>
+              <a href="#about" className="btn btn-primary mt-3">Saber más</a>
+            </div>
+    
+            {/* Imagen a la derecha */}
+            <div className="col-md-6 text-center">
+              <img
+                src="/src/assets/images/Imagen 1.jpg"
+                alt="Empresa"
+                className="img-fluid rounded "
+                width="80%"
+              />
+            </div>
+          </div>
+        </section>
+      );
 }
 
 export default HomePage
